875a38c41f5ea91a1340094b70615a7dfc522b35
Gituserrr75 <93101775+Gituserrr75@users.noreply.github.com>
2022-01-29 13:19:00 -0800

    Add files via upload

--- README.md
+++ README.md
@@ -8,8 +8,8 @@ converting a web-page from a static project to an interactive one.
 - used javascript to add a navigation bar to the different sections
 - select active section when scrolling 
 - made page responsive for smaller screens 
-
+- added a scroll to top button
 ## Resources 
-- https://www.w3schools.com/default.asp
-- https://developer.mozilla.org/en-US/
 - https://stackoverflow.com/questions/63266291/highlighting-navbar-list-when-element-is-in-viewport-using-vanilla-javascript
+- https://dev.to/rohank_2502/implementing-smooth-scroll-using-javascript-28n3
+- https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
--- js/app.js
+++ js/app.js
@@ -1,47 +1,75 @@
-
-
 // Global Variables
-const navBar = document.getElementById("navbar__list");
-const sections = document.querySelectorAll("section");
+const nav = document.getElementById("navbar__list");
 const fragment = document.createDocumentFragment();
+const sections = document.querySelectorAll("section");
 
 // Creating navigation bar list 
-function createlist() {
+ function create() {
   for (const section of sections) {
     const item = document.createElement("li");
     const itemLink = document.createElement("a");
     itemLink.innerHTML = `<a href="#${section.id}" class="menu__link">${section.dataset.nav}</a>`;
-    itemLink.addEventListener("click", (event) => {
-      event.preventDefault();
-      section.scrollIntoView({ 
-        behavior: "smooth" , 
-     block: 'center'
-    });
-    });
     item.appendChild(itemLink);
     fragment.appendChild(item);
   }
-  navBar.appendChild(fragment);
+  nav.appendChild(fragment);
 }
-createlist();
+create();
 
-// getting BoundingClientRect of viewport
-function active(section) {
-    const activeElement = section.getBoundingClientRect();
-    return activeElement.top > -100 && activeElement.top <= 500
+// getBoundingClientRect of viewport
+function act(section){
+  const act = section.getBoundingClientRect();
+  return act.top > -100 && act.top <= 500
 }
 
-//   setting active section and nav-bar highlight when in viewport
-window.addEventListener("scroll", makeActive);
-function makeActive() {
-  for(const section of sections) {
+// Make Active class 
+window.addEventListener('scroll', ()=>{
+  for (const section of sections) {
+    if (act(section)){
+      section.classList.add('your-active-class');
+    } else {
+      section.classList.remove('your-active-class');
+    }
+  }
+})
+
+// highlight the section in viewport (active section)
+window.addEventListener("scroll", () =>{
+  for (const section of sections) {
     const link = document.querySelector(`a[href="#${section.id}"]`);
-    if (active(section)) {
-     section.classList.add("your-active-class");
+    if (act(section)) {
       link.classList.add("active");
     } else {
-     section.classList.remove("your-active-class");
      link.classList.remove("active");
     }
   }
+})
+
+// smooth scrolling 
+nav.addEventListener('click', (e) => {
+  e.preventDefault();
+  const selected = e.target;
+  if (selected.classList.contains('menu__link')) {
+      const id = selected.getAttribute('href').slice(1);
+      document.getElementById(id).scrollIntoView({ 
+        behavior: 'smooth',
+        block: 'center'
+       });
+  }
+});
+
+//display go to top button when scrolling 
+window.addEventListener('scroll', () =>{
+  const top = document.getElementById('toTop');
+  if (document.body.scrollTop > 550 || document.documentElement.scrollTop > 550) {
+    top.style.display = "block";
+  } else {
+    top.style.display = "none";
+  }
+})
+
+// scroll to top when button clicked
+function goTop() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0;
 }
